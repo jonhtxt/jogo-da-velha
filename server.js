8aa93ca556513cd5640423d853cb0b4f75859a78
Jonata <macedojonata254@gmail.com>
2025-04-12 12:35:10 -0300

    Organiza arquivos na pasta public

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
 
+
 // Servir arquivos estáticos da pasta public
 app.use(express.static(path.join(__dirname, 'public')));
 
